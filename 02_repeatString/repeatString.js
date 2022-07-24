2c2177a04583da91145b30e37069d93b8479e8e2
Eric Lee <79837982+EricLee911110@users.noreply.github.com>
2022-07-24 22:55:43 +0800

    repeating string and reversing string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(keyword, repeat_times) {
+    if (repeat_times < 0){
+        return 'ERROR';
+    } else {
+        let res = '';
+        for ( i = 1; i <= repeat_times; i++){
+            res += keyword;
+        }
+        return res;
+    };
+
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const setCharAt = function(string, index, chr){
+    return string.substring(0,index) + chr + string.substring(index+1, string.length);
+}
 
+const reverseString = function(string) {
+    let l = string.length - 1;
+
+    for ( let i = 0; i < l; i++ ){
+        let rightChar = string[l];
+        let leftChar = string[i];
+        string = setCharAt(string, i, rightChar);
+        string = setCharAt(string, l, leftChar);
+        console.log(string)
+        l--;
+    }
+    
+    return string
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
